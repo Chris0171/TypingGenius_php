5a61fe0100bea12d1f3af26eccaec1103fbf1f1f
Christian Milanes <christianmilanes01@gmail.com>
2024-04-21 01:49:28 +0200

    head include added

---
+++ includes/head.inc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>TypingGenius</title>
+		<link rel="icon" href="assets/favicon.ico" type="image/x-icon" />
+
+		<!-- Bootstrap styles -->
+		<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+			integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+			crossorigin="anonymous"
+		/>
+
+		<link rel="stylesheet" href="assets/CSS/styles.css" />
+
+		<!-- Google adsense -->
+		<script
+			async
+			src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6220548139750526"
+			crossorigin="anonymous"
+		></script>
+	</head>
+
+	<body>
