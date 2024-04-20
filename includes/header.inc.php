e97a0bf1ffbb5c5767d51271ee6ca01a8dd1fc65
Christian Milanes <christianmilanes01@gmail.com>
2024-04-21 01:51:03 +0200

    header include created

---
+++ includes/header.inc.php
@@ -0,0 +1,19 @@
+<header class="navbar navbar-expand-lg navbar-dark bgNav p-2 pe-lg-4 ps-lg-4 pe-md-3 ps-md-3 justify-content-center">
+  <!-- Navbar -->
+  <nav class="container-xxl">
+    <a class="navbar-brand fw-bold ms-2 ms-md-3" href="#"><img class="img-fluid" src="assets/favicon.ico"
+    width="50" alt="ContentSynth-Logo" width="85"> Content-Synth</a>    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+      <span class="navbar-toggler-icon text-white"></span>
+    </button>
+    <div class="navbar-collapse collapse" id="menu">
+      <nav class="navbar-nav ms-md-auto">
+        <a class="nav-link fw-bold active" href="#">INICIO</a>
+        <a class="nav-link fw-bold active" href="#">SOBRE NOSOTROS</a>
+        <a class="nav-link fw-bold active" href="#">INTRUCCIONES</a>
+        <a class="nav-link fw-bold active" href="#">PARTIDA</a>
+        <a class="nav-link fw-bold active" href="#">PUNTUACIONES</a>
+      </nav>
+    </div>
+  </nav>
+</header>
