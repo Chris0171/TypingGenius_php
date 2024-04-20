593f2c1bda24496ed07b6f24600399e5ab160072
Christian Milanes <christianmilanes01@gmail.com>
2024-04-21 01:51:35 +0200

    index page created

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include ('includes/head.inc.php');
+?>
+
+<div class="container-fluid p-0 g-0">
+  <?php
+  include ('includes/header.inc.php');
+  ?>
+</div>
+
+
+<?php
+include ('includes/end.inc.php');
+?>
