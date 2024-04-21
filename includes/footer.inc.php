e7f200dc0efe04b43fcdf552d7053f5b4ceee56c
Christian Milanes <christianmilanes01@gmail.com>
2024-04-21 15:16:07 +0200

    footer added and implemented

---
+++ includes/footer.inc.php
@@ -0,0 +1,11 @@
+<footer>
+  <div class="row justify-content-center p-3 m-0 mt-5 bgNav">
+    <div class="col-md-4">
+      <p class="fw-bold subMenu">Sitio</p>
+      <a href="" class="fw-bold footerLink">Política de Privacidad</a><br>
+      <a href="" class="fw-bold footerLink">Terminos de uso</a><br>
+      <a href="" class="fw-bold footerLink">Contacto</a><br>
+      <a href="" class="fw-bold footerLink">Cookies</a><br>
+    </div>
+  </div>
+</footer>
--- index.php
+++ index.php
@@ -1,14 +1,21 @@
+<!-- Head -->
 <?php
 include ('includes/head.inc.php');
 ?>
 
 <div class="container-fluid p-0 g-0">
+  <!-- Header -->
   <?php
   include ('includes/header.inc.php');
   ?>
+
+  <!-- Footer -->
+  <?php
+  include ('includes/footer.inc.php');
+  ?>
 </div>
 
-
+<!-- End -->
 <?php
 include ('includes/end.inc.php');
 ?>
