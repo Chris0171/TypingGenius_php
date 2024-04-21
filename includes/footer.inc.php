67c46c4722a6c4b25aff5fced6bf1089d657564c
Christian Milanes <christianmilanes01@gmail.com>
2024-04-21 23:16:05 +0200

    new elements added

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,11 +1,24 @@
-<footer>
-  <div class="row justify-content-center p-3 m-0 mt-5 bgNav">
+<footer class="m-0 mt-5 bgNav">
+  <div class="row justify-content-center p-3 m-0">
     <div class="col-md-4">
       <p class="fw-bold subMenu">Sitio</p>
-      <a href="" class="fw-bold footerLink">Política de Privacidad</a><br>
-      <a href="" class="fw-bold footerLink">Terminos de uso</a><br>
-      <a href="" class="fw-bold footerLink">Contacto</a><br>
-      <a href="" class="fw-bold footerLink">Cookies</a><br>
+      <a href="#" class="fw-bold footerLink">Política de Privacidad</a><br>
+      <a href="#" class="fw-bold footerLink">Terminos de uso</a><br>
+      <a href="#" class="fw-bold footerLink">Contacto</a><br>
+      <a href="#" class="fw-bold footerLink">Cookies</a><br>
+      <a href="#" class="fw-bold footerLink">Preguntas frecuentes</a><br>
+    </div>
+    <div class="col-md-4">
+      <p class="fw-bold subMenu">Otras opciones</p>
+      <a href="https://agilefingers.com/" class="fw-bold footerLink">Agile fingers</a><br>
+      <a href="" class="fw-bold footerLink">Cursos</a><br>
+      <a href="" class="fw-bold footerLink">Juegos</a><br>
+      <a href="" class="fw-bold footerLink">Pruebas</a><br>
+    </div>
+  </div>
+  <div class="row justify-content-center align-items-center g-0 bg-footer-end">
+    <div class="col-12 text-white p-3 text-center fw-medium">
+      © 2024 by Christian Milanés Rodríguez.
     </div>
   </div>
 </footer>
