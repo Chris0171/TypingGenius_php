37253383c49d0b035518631e97fba42623adb067
Christian Milanes <christianmilanes01@gmail.com>
2024-04-21 23:19:47 +0200

    index inc created

---
+++ includes/index.inc.php
@@ -0,0 +1,89 @@
+<div class="row justify-content-center align-items-center mt-5">
+  <div class="col-10">
+    <div class="card card-bg-tech">
+      <div class="card-body">
+        <h5 class="card-title text-center">Niveles de mecanografía</h5>
+        <div class="row justify-content-center align-items-center mb-4 mt-4 text-center">
+          <div class="col-12 col-md-2">
+            <button class="level-box level-1">Principiante</button>
+          </div>
+          <div class="col-12 col-md-2">
+            <button class="level-box level-2">Intermedio</button>
+          </div>
+          <div class="col-12 col-md-2">
+            <button class="level-box level-3">Avanzado</button>
+          </div>
+          <div class="col-12 col-md-2">
+            <button class="level-box level-4">Profesional</button>
+          </div>
+          <div class="col-12 col-md-2">
+            <button class="level-box level-5">Experto</button>
+          </div>
+        </div>
+        <div class="text-center">
+          <a href="#" class="btn btn-primary fw-bold">Practica ahora</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row justify-content-center align-items-center mt-3">
+  <div class="col-11 col-md-5 mb-3 mb-md-0">
+    <div class="card card-bg-tech">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-11 col-md-5 mb-3 mb-md-0">
+    <div class="card card-bg-tech">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row justify-content-center align-items-center mt-3">
+  <div class="col-11 col-md-5 mb-3 mb-md-0">
+    <div class="card card-bg-tech">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-11 col-md-5 mb-3 mb-md-0">
+    <div class="card card-bg-tech">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row justify-content-center align-items-center mt-3">
+  <div class="col-11 col-md-5 mb-3 mb-md-0">
+    <div class="card card-bg-tech">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-11 col-md-5 mb-3 mb-md-0">
+    <div class="card card-bg-tech">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
