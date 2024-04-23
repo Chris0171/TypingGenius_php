11b7f3b499e7dc207adba1952d3c77011914986a
Christian Milanes <christianmilanes01@gmail.com>
2024-04-23 07:18:28 +0200

    new card added

--- includes/index.inc.php
+++ includes/index.inc.php
@@ -1,4 +1,4 @@
-<div class="row justify-content-center align-items-center mt-5">
+<div class="row justify-content-center align-items-center m-0 mt-5">
   <div class="col-10">
     <div class="card card-bg-tech">
       <div class="card-body">
@@ -27,62 +27,53 @@
     </div>
   </div>
 </div>
-<div class="row justify-content-center align-items-center mt-3">
+<div class="row justify-content-center align-items-center m-0 mt-3">
   <div class="col-11 col-md-5 mb-3 mb-md-0">
     <div class="card card-bg-tech">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Práctica regular</h5>
+        <p class="card-text">La práctica constante es fundamental para mejorar la velocidad y precisión en la
+          mecanografía. Dedica más tiempo a practicar la escritura, preferiblemente todos los días. Puedes
+          utilizar recursos como programas en línea, libros de ejercicios o aplicaciones especializadas que te
+          proporcionen ejercicios de mecanografía para practicar. Una plataforma recomendada sería TypingClub.</p>
+        <a href=https://www.typingclub.com/ target="_blank" class="btn btn-primary">Visitar TypingClub</a>
       </div>
     </div>
   </div>
-  <div class="col-11 col-md-5 mb-3 mb-md-0">
+  <div class="col-11 col-md-5 m-0 mb-3 mb-md-0">
     <div class="card card-bg-tech">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="row justify-content-center align-items-center mt-3">
-  <div class="col-11 col-md-5 mb-3 mb-md-0">
-    <div class="card card-bg-tech">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-11 col-md-5 mb-3 mb-md-0">
-    <div class="card card-bg-tech">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Aprender la técnica adecuada</h5>
+        <p class="card-text">Aprender y practicar la técnica adecuada de mecanografía es esencial para mejorar tu
+          velocidad y precisión. Esto implica mantener una postura ergonómica, colocar los dedos correctamente en el
+          teclado (utilizando todos los dedos y sin mirar las teclas), y usar la técnica de "toque" en lugar de mirar y
+          buscar las letras. Una heramienta recomendada sería Keybr.</p>
+        <a href=https://www.keybr.com/ target="_blank" class="btn btn-primary">Visitar Keybr</a>
       </div>
     </div>
   </div>
 </div>
-<div class="row justify-content-center align-items-center mt-3">
+<div class="row justify-content-center m-0 align-items-center mt-3">
   <div class="col-11 col-md-5 mb-3 mb-md-0">
     <div class="card card-bg-tech">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Realizar ejercicios de velocidad y precisión</h5>
+        <p class="card-text">Además de practicar regularmente, realiza ejercicios específicos para mejorar tanto la
+          velocidad como la precisión. Puedes realizar pruebas cronometradas para medir tu velocidad de escritura y
+          realizar ejercicios de corrección de errores para mejorar la precisión. Una plataforma recomendada sería
+          Typing.com.</p>
+        <a href=https://www.typing.com/ target="_blank" class="btn btn-primary">Visitar Typing</a>
       </div>
     </div>
   </div>
   <div class="col-11 col-md-5 mb-3 mb-md-0">
     <div class="card card-bg-tech">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Participar en desafíos y competencias:</h5>
+        <p class="card-text">Únete a desafíos de mecanografía en línea o participa en competencias de velocidad de
+          escritura. Estos eventos pueden motivarte a mejorar tus habilidades y proporcionarte una medida objetiva de tu
+          progreso. Una plataforma recomendada sería Nitro Type.</p>
+        <a href=https://www.nitrotype.com/ target="_blank" class="btn btn-primary">Visitar Nitro Type</a>
       </div>
     </div>
   </div>
