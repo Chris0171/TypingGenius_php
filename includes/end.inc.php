30feda0ae076599f82e1c263b7247479fe76060c
Christian Milanes <christianmilanes01@gmail.com>
2024-04-27 18:25:13 +0200

    contact popover implemented

--- includes/end.inc.php
+++ includes/end.inc.php
@@ -1,13 +1,14 @@
-		<!-- Bootstrap scripts -->
-		<script
-			src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-			integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-			crossorigin="anonymous"
-		></script>
-		<script
-			src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-			integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-			crossorigin="anonymous"
-		></script>
-	</body>
+<!-- Bootstrap scripts -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+	integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+	integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<script>
+	const tooltips = document.querySelectorAll('.tooltipBtn')
+	tooltips.forEach(t => {
+		new bootstrap.Popover(t);
+	});
+</script>
+</body>
+
 </html>
--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,19 +1,36 @@
 <footer class="m-0 mt-5 bgNav">
-  <div class="row justify-content-center p-3 m-0">
-    <div class="col-md-4">
-      <p class="fw-bold subMenu">Sitio</p>
-      <a href="privacy-policy.php" class="fw-bold footerLink">Política de Privacidad</a><br>
-      <a href="terms-of-use.php" class="fw-bold footerLink">Terminos de uso</a><br>
-      <a href="#" class="fw-bold footerLink">Contacto</a><br>
-      <a href="cookies-policy.php" class="fw-bold footerLink">Cookies</a><br>
-      <a href="#" class="fw-bold footerLink">Preguntas frecuentes</a><br>
+  <div class="row justify-content-center align-items-center p-3 m-0">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+      <p class="fw-bold subMenu text-warning">Sitio</p>
+      <ul>
+        <li><a href="privacy-policy.php" class="fw-bold footerLink">Política de Privacidad</a></li>
+        <li><a href="terms-of-use.php" class="fw-bold footerLink">Terminos de uso</a></li>
+        <li><a href="cookies-policy.php" class="fw-bold footerLink">Cookies</a></li>
+        <li><a href="#" class="fw-bold footerLink">Preguntas frecuentes</a></li>
+      </ul>
     </div>
-    <div class="col-md-4">
-      <p class="fw-bold subMenu">Otras opciones</p>
-      <a href="https://agilefingers.com/" class="fw-bold footerLink">Agile fingers</a><br>
-      <a href="" class="fw-bold footerLink">Cursos</a><br>
-      <a href="" class="fw-bold footerLink">Juegos</a><br>
-      <a href="" class="fw-bold footerLink">Pruebas</a><br>
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+      <p class="fw-bold subMenu text-warning">Otras opciones</p>
+      <ul>
+        <li><a href="https://agilefingers.com/" class="fw-bold footerLink">Agile fingers</a></li>
+        <li><a href="" class="fw-bold footerLink">Cursos</a></li>
+        <li><a href="" class="fw-bold footerLink">Juegos</a></li>
+        <li><a href="" class="fw-bold footerLink">Pruebas</a></li>
+      </ul>
+    </div>
+    <div class="col-12 col-sm-12 col-md-4 col-lg-3 pt-md-3">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-5 col-md-12">
+          <button type="button" class="fw-bold footerLink tooltipBtn" data-bs-trigger="hover focus" data-bs-toggle="popover"
+            data-bs-title="Email de Contacto" data-bs-custom-class="fw-bold bg-warning-subtle bg-gradient"
+            data-bs-content="christianmilanes01@gmail.com"><i
+              class="bi bi-envelope-at-fill pe-2 footIcon"></i>Contacto</button><br>
+        </div>
+        <div class="col-5 col-md-12">
+          <a href="https://github.com/Chris0171" target="_blank" class="fw-bold footerLink"><i
+              class="bi bi-github pe-2 footIcon"></i>Repositorio</a><br>
+        </div>
+      </div>
     </div>
   </div>
   <div class="row justify-content-center align-items-center g-0 bg-footer-end">
