905e3a18d55a9067fd6b6cb0d508a0de3d54d9e3
Christian Milanes <christianmilanes01@gmail.com>
2024-04-27 17:07:30 +0200

    cookies policy page created

---
+++ cookies-policy.php
@@ -0,0 +1,82 @@
+<!-- Head start -->
+<?php
+include ('includes/head_start.inc.php');
+?>
+
+<!-- Imports -->
+<link rel="stylesheet" href="assets/CSS/styles.css" />
+
+<?php
+include ('includes/head_end.inc.php');
+?>
+
+<div class="container-fluid p-0 g-0">
+  <!-- Header -->
+  <?php
+  include ('includes/header.inc.php');
+  ?>
+  <!-- page content -->
+  <div class="row justify-content-center m-0 align-items-center mt-5">
+    <div class="col-12 col-md-10 card-bg-tech p-5">
+      <h2 class="fw-bold text-warning">Política de cookies</h2>
+      <p class="fw-bold">Por favor, lea detenidamente los siguientes términos antes de utilizar <span
+          class="text-info">TypingGenius</span>:
+      </p>
+      <ol>
+        <li>
+          <span class="fw-bold text-warning">¿Qué son las cookies?</span>
+          <p>Las cookies son pequeños archivos de texto que se almacenan en su dispositivo (computadora, teléfono
+            móvil u otro dispositivo) cuando visita nuestro sitio web. Las cookies nos permiten reconocer su
+            dispositivo y mejorar su experiencia de usuario en nuestro sitio web.</p>
+        </li>
+        <li>
+          <span class="fw-bold text-warning">Tipos de cookies que utilizamos</span>
+          <ul>
+            <li> Cookies Esenciales: Estas cookies son esenciales para proporcionarle servicios disponibles a través de
+              nuestro sitio web y permitirle utilizar algunas de sus funciones. Por ejemplo, estas cookies le permiten
+              iniciar sesión en áreas seguras de nuestro sitio web.</li>
+            <li>Cookies de Rendimiento y Análisis: Utilizamos estas cookies para recopilar información sobre cómo los
+              visitantes interactúan con nuestro sitio web, incluyendo las páginas que visitan y los enlaces en los que
+              hacen clic. Esta información se utiliza para mejorar el funcionamiento de nuestro sitio web y entender
+              mejor las necesidades de nuestros usuarios.</li>
+            <li> Cookies de Funcionalidad: Estas cookies se utilizan para recordar las preferencias del usuario y
+              proporcionar funcionalidades mejoradas. Por ejemplo, estas cookies pueden recordar su nombre de usuario o
+              elección de idioma en nuestro sitio web.</li>
+          </ul>
+        </li>
+        <li>
+          <span class="fw-bold text-warning">Control de cookies</span>
+          <p>Puede controlar y administrar el uso de cookies en su dispositivo a través de la configuración de su
+            navegador. Tenga en cuenta que deshabilitar ciertas cookies puede afectar la funcionalidad de nuestro sitio
+            web y limitar su experiencia de usuario.</p>
+        </li>
+        <li>
+          <span class="fw-bold text-warning">Cookies de terceros</span>
+          <p>Es posible que terceros, como proveedores de servicios de análisis, publicidad u otros servicios, utilicen
+            cookies en nuestro sitio web para proporcionar servicios relevantes para usted. Estas cookies son
+            gestionadas por terceros y están sujetas a las políticas de privacidad y cookies de dichos terceros.</p>
+        </li>
+        <li>
+          <span class="fw-bold text-warning">Actualización de la política de cookies</span>
+          <p>Nos reservamos el derecho de actualizar esta Política de Cookies en cualquier momento. Se le notificará
+            sobre cualquier cambio mediante la publicación de la Política de Cookies actualizada en nuestro sitio web.
+          </p>
+        </li>
+        <li>
+          <span class="fw-bold text-warning">Contacto</span>
+          <p>Si tiene alguna pregunta sobre nuestra Política de Cookies, por favor contáctenos a través de <span
+              class="fw-bold text-info">(christianmilanes01@gmail.com)</span>.</p>
+        </li>
+      </ol>
+    </div>
+  </div>
+  <!-- Footer -->
+  <?php
+  include ('includes/footer.inc.php');
+  ?>
+</div>
+
+<!-- End -->
+<?php
+include ('includes/end.inc.php');
+?>
