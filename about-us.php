ee24d5727221c7817ddbe1f84469927035e5c187
Christian Milanes <christianmilanes01@gmail.com>
2024-04-28 13:46:21 +0200

    about-us page created

---
+++ about-us.php
@@ -0,0 +1,55 @@
+<!-- Head start -->
+<?php
+include ('includes/head_start.inc.php');
+?>
+
+<!-- Imports -->
+<link rel="stylesheet" href="assets/CSS/styles.css" />
+
+<?php
+include ('includes/head_end.inc.php');
+?>
+
+<div class="container-fluid p-0 g-0">
+  <!-- Header -->
+  <?php
+  include ('includes/header.inc.php');
+  ?>
+  <!-- page content -->
+  <div class="row justify-content-center m-0 align-items-center mt-5">
+    <div class="col-12 col-md-10 card-bg-tech p-5">
+      <h2 class="fw-bold text-warning">Sobre nosostros</h2>
+      <p class="fw-bold">Bienvenido a <span class="text-info">TypingGenius</span>, tu destino en línea para mejorar tus
+        habilidades de mecanografía de manera efectiva y divertida.</p><br>
+      <h4>Nuestra misión</h4>
+      <p>En <span class="text-info">TypingGenius</span>, nuestra misión es proporcionar a nuestros usuarios las
+        herramientas y recursos necesarios para dominar la habilidad de mecanografía de manera eficiente y efectiva.
+        Creemos en la importancia de la mecanografía en el mundo digital actual y estamos comprometidos a ayudar a las
+        personas a alcanzar su máximo potencial en esta área.</p><br>
+      <h4>Qué ofrecemos</h4>
+      <p>En nuestro sitio web, encontrarás una variedad de recursos, incluyendo tutoriales, prácticas interactivas,
+        juegosdivertidos y más, diseñados para ayudarte a mejorar tu velocidad y precisión en la mecanografía. Ya sea
+        que estés buscando mejorar tus habilidades básicas de mecanografía o aspiras a convertirte en un experto en el
+        teclado, <span class="text-info">TypingGenius</span> tiene algo para ti.
+      </p><br>
+      <h4>Nuestro equipo</h4>
+      <p>En <span class="text-info">TypingGenius</span>, contamos con un equipo apasionado de expertos en educación,
+        diseño web y desarrollo de software, comprometidos a brindarte la mejor experiencia posible. Nos enorgullece
+        ofrecer un sitio web fácil de usar, con contenido de alta calidad y un servicio al cliente excepcional.</p><br>
+      <h4>Contáctanos</h4>
+      <p>Si tienes alguna pregunta, comentario o sugerencia, no dudes en ponerte en contacto con nosotros. Estamos aquí
+        para ayudarte en tu viaje hacia la mejora de tus habilidades de mecanografía.</p><br>
+      <p>Gracias por elegir <span class="text-info">TypingGenius</span> como tu recurso para mejorar tus habilidades de
+        mecanografía. ¡Esperamos que disfrutes tu experiencia con nosotros!</p>
+    </div>
+  </div>
+  <!-- Footer -->
+  <?php
+  include ('includes/footer.inc.php');
+  ?>
+</div>
+
+<!-- End -->
+<?php
+include ('includes/end.inc.php');
+?>
