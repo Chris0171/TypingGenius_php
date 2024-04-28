3c893231e4d833053e33c13c9ca4cd6aeb595eb6
Christian Milanes <christianmilanes01@gmail.com>
2024-04-28 14:03:24 +0200

    lean more content created

---
+++ includes/learn-more.inc.php
@@ -0,0 +1,234 @@
+<div class="row m-0">
+  <!-- Left col -->
+  <div class="col-3 col-md-3 p-0 menu">
+    <div class="accordion accordion-flush">
+      <!-- Item 1 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapse_1">
+            INTRODUCCIÓN
+          </button>
+        </h2>
+        <div id="collapse1" class="accordion-collapse collapse show">
+          <div class="accordion-body p-0">
+            <div class="infoLink link-active">
+              <p class="text-black">
+                Qué es la mecanografía y su importancia en la era digital.
+              </p>
+            </div>
+            <div class="infoLink">
+              <p class="text-black">
+                Beneficios de aprender a mecanografiar correctamente.
+              </p>
+            </div>
+            <div class="infoLink">
+              <p class="text-black">
+                Origen y desarrollo de las técnicas.
+              </p>
+            </div>
+            <div class="infoLink">
+              <p class="text-black">
+                Hitos importantes en la historia de la mecanografía.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Item 2 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse_2">
+            TÉCNICAS Y MÉTODOS
+          </button>
+        </h2>
+        <div id="collapse2" class="accordion-collapse collapse">
+          <div class="accordion-body p-0">
+            <div class="infoLink">
+              <a href="#" class="text-black">Método de tocar sin mirar el teclado.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Comparación y recomendaciones de diferentes métodos.</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Item 3 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse_4">
+            POSTURA Y ERGONOMÍA
+          </button>
+        </h2>
+        <div id="collapse3" class="accordion-collapse collapse">
+          <div class="accordion-body p-0">
+            <div class="infoLink">
+              <a href="#" class="text-black">Importancia de mantener una postura correcta al
+                mecanografiar.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Consejos para prevenir lesiones por esfuerzo
+                repetitivo.</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Item 4 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse_4">
+            PRÁCTICA Y ENTRENAMIENTO
+          </button>
+        </h2>
+        <div id="collapse4" class="accordion-collapse collapse">
+          <div class="accordion-body p-0">
+            <div class="infoLink">
+              <a href="#" class="text-black">Ejercicios básicos para mejorar velocidad y precisión.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Recursos para practicar la colocación de los dedos en el
+                teclado.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Ejemplos y técnicas para mejorar la velocidad de
+                mecanografía.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Enfoques para superar obstáculos y mejorar la
+                precisión.</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Item 5 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse_4">
+            APLICACIÓN DE LA MECANOGRAFÍA
+          </button>
+        </h2>
+        <div id="collapse5" class="accordion-collapse collapse">
+          <div class="accordion-body p-0">
+            <div class="infoLink">
+              <a href="#" class="text-black">Importancia de la mecanografía en el trabajo y la
+                productividad.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Habilidades requeridas en diferentes profesiones que
+                valoran la mecanografía.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Rol de la mecanografía en la educación y el desarrollo de
+                habilidades.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Estrategias para enseñar mecanografía de manera efectiva
+                a estudiantes.</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Item 6 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapse_4">
+            HERRAMIENTAS Y RECURSOS
+          </button>
+        </h2>
+        <div id="collapse6" class="accordion-collapse collapse">
+          <div class="accordion-body p-0">
+            <div class="infoLink">
+              <a href="#" class="text-black">Revisión de herramientas y programas para practicar
+                mecanografía.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Evolución de los teclados y su impacto en la
+                mecanografía.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Teclados ergonómicos y otras tecnologías relacionadas.</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Item 7 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse7" aria-expanded="false" aria-controls="collapse_4">
+            BENEFICIOS Y MITOS
+          </button>
+        </h2>
+        <div id="collapse7" class="accordion-collapse collapse">
+          <div class="accordion-body p-0">
+            <div class="infoLink">
+              <p class="text-black">
+                Razones por las que aprender mecanografía es útil en la
+                vida diaria.
+              </p>
+            </div>
+            <div class="infoLink">
+              <p class="text-black">
+                Ventajas en la velocidad y eficiencia al usar el teclado.
+              </p>
+            </div>
+            <div class="infoLink">
+              <p class="text-black">
+                Mitos Comunes sobre la Mecanografía
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Item 8 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse8" aria-expanded="false" aria-controls="collapse_4">
+            DESARROLLO PERSONAL
+          </button>
+        </h2>
+        <div id="collapse8" class="accordion-collapse collapse">
+          <div class="accordion-body p-0">
+            <div class="infoLink">
+              <a href="#" class="text-black">Impacto de la mecanografía en el desarrollo cognitivo y
+                la concentración.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Relación entre la mecanografía y el desarrollo de
+                habilidades cerebrales.</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Item 9 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse9" aria-expanded="false" aria-controls="collapse_4">
+            MECANOGRAFÍA PARA DIFERENTES GRUPOS
+          </button>
+        </h2>
+        <div id="collapse9" class="accordion-collapse collapse">
+          <div class="accordion-body p-0">
+            <div class="infoLink">
+              <a href="#" class="text-black">Importancia de enseñar mecanografía a una edad
+                temprana.</a>
+            </div>
+            <div class="infoLink">
+              <a href="#" class="text-black">Recursos y métodos adaptados para enseñar mecanografía a
+                niños y adolescentes.</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Right col -->
+  <div class="col-9 p-0 menu bg-dark bg-gradient content" id="rightCol"></div>
+</div>
