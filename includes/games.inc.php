978e5af1760bcf44313324147a60ad9e6a23a204
Christian Milanes <christianmilanes01@gmail.com>
2024-04-28 03:16:28 +0200

    games inc created

---
+++ includes/games.inc.php
@@ -0,0 +1,81 @@
+<div class="row justify-content-center m-0 align-items-center mt-5">
+  <div class="col-11 offset-md-1 text-white mb-3">
+    <h2>Juegos para practicar</h2>
+  </div>
+</div>
+<div class="row justify-content-center m-0 align-items-center">
+  <div class="col-12 col-md-5 m-0 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Saltador de palabras <span class="text-info fw-bold">[Agile Fingers]</span></h5>
+        <p class="card-text">Escribirás palabras completas en este juego de mecanografía. Si escribes una palabra
+          correctamente, tu saltador salta diferentes obstáculos y lucha contra los enemigos. Este juego de mecanografía
+          tiene múltiples niveles que difieren en dificultad.</p><br>
+        <div class="text-center">
+          <a href="https://agilefingers.com/es/juegos/saltador-de-palabras" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-12 col-md-5 m-0 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Rescate de ovejas <span class="text-info fw-bold">[Agile Fingers]</span></h5>
+        <p class="card-text">Aprenda a escribir caracteres individuales. Este juego de mecanografía es bueno para
+          principiantes. El apocalipsis de los extraterrestres está a nuestras puertas. ¡No dejes que se lleven nuestras
+          ovejas! Cuanto más alto sea el nivel, más ovejas habrá que rescatar.</p><br>
+        <div class="text-center">
+          <a href="https://agilefingers.com/es/juegos/rescate-de-ovejas" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row justify-content-center m-0 align-items-center">
+  <div class="col-12 col-md-5 m-0 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Palabras estrella <span class="text-info fw-bold">[Agile Fingers]</span></h5>
+        <p class="card-text">Aprenda a escribir palabras completas. Consigue tantas estrellas como puedas escribir las
+          palabras escritas en ellas. Este juego de mecanografía se trata de velocidad y de conseguir tantos puntos como
+          sea posible. Escribe las palabras asignadas a las estrellas antes de que exploten.</p><br>
+        <div class="text-center">
+          <a href="https://agilefingers.com/es/juegos/palabras-estrella" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-12 col-md-5 m-0 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Órbita <span class="text-info fw-bold">[Midudev]</span></h5>
+        <p class="card-text">Órbita es un sencillo juego de mecanografía desarrollado con HTML, CSS y Javascript puro.
+          Eres el nuevo capitán de una nave espacial y tendrás que luchar contra una lluvia de palabras para salvar al
+          universo de una mala escritura con tu teclado.</p><br>
+        <div class="text-center">
+          <a href="https://programacion-es.dev/orbita/" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- <div class="row m-0 align-items-center">
+  <div class="col-12 col-md-5 offset-md-1 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Palabras estrella <span class="text-info fw-bold">[Agile Fingers]</span></h5>
+        <p class="card-text">Aprenda a escribir palabras completas. Consigue tantas estrellas como puedas escribir las
+          palabras escritas en ellas. Este juego de mecanografía se trata de velocidad y de conseguir tantos puntos como
+          sea posible. Escribe las palabras asignadas a las estrellas antes de que exploten.</p><br>
+        <div class="text-center">
+          <a href="https://agilefingers.com/es/juegos/palabras-estrella" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
