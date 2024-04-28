a6962556690fc374e213461f82a4ded07b2b66f3
Christian Milanes <christianmilanes01@gmail.com>
2024-04-28 18:20:34 +0200

    match page created

---
+++ match.php
@@ -0,0 +1,40 @@
+<!-- Head start -->
+<?php
+include ('includes/head_start.inc.php');
+?>
+
+<!-- Imports -->
+<link rel="stylesheet" href="assets/CSS/styles.css" />
+<!-- Google Fonts links -->
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Electrolize&display=swap" rel="stylesheet" />
+
+<?php
+include ('includes/head_end.inc.php');
+?>
+
+<div class="container-fluid p-0 g-0">
+  <!-- Header -->
+  <?php
+  include ('includes/header.inc.php');
+  ?>
+  <div class="m-0 p-0 mb-5">
+    <!-- page content -->
+    <?php
+    include ("includes/match.inc.php");
+    ?>
+  </div>
+  <!-- Footer -->
+  <?php
+  include ('includes/footer.inc.php');
+  ?>
+</div>
+
+<!-- Proyect script -->
+<script type="module" src="/assets/JS/match.js"></script>
+
+<!-- End -->
+<?php
+include ('includes/end.inc.php');
+?>
