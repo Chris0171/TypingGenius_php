eafb85133f6a5585e903e001ab60f146c4922988
Christian Milanes <christianmilanes01@gmail.com>
2024-04-28 11:32:01 +0200

    testing content created

---
+++ includes/testing.inc.php
@@ -0,0 +1,86 @@
+<div class="row justify-content-center m-0 align-items-center mt-5">
+  <div class="col-11 offset-md-1 text-white mb-3">
+    <h2>Juegos para practicar</h2>
+  </div>
+</div>
+<div class="row justify-content-center m-0 align-items-center">
+  <div class="col-12 col-md-5 m-0 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Pruebas de mecanografía <span class="text-info fw-bold">[KeyHero]</span></h5>
+        <p class="card-text">Prueba de mecanografía con PPM, precisión y gráficos. Para una mejor experiencia se
+          recomienda crear una cuenta (registrarse), ya que puedes guardar tus estadísticas y conocer tu progreso a
+          medida que practicas. Una forma muy fácil de evolucionar en tus objetivos. Tiene soporte en <span
+            class="text-warning">10</span> indiomas.</p><br>
+        <div class="text-center">
+          <a href="https://www.keyhero.com/prueba-mecanografia/test-gratuito/" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-12 col-md-5 m-0 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Pruebas de mecanografía <span class="text-info fw-bold">[Agile Fingers]</span></h5>
+        <p class="card-text">Este sitio además de ofrecer cursos y lecciones, tiene un apartado para pruebas y prácticas
+          de velocidad. Si te creas una cuenta de usuario puedes visualizar tu progreso en la misma sección de pruebas.
+          Esto de dejará claras tus habilidades actuales y podrás fijar objetivos a cumplir que te ayudarán a mejorar
+          mucho más rapido.</p><br>
+        <div class="text-center">
+          <a href="https://agilefingers.com/es/juegos/rescate-de-ovejas" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row justify-content-center m-0 align-items-center">
+  <div class="col-12 col-md-5 m-0 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Pruebas de mecanografía en <span class="text-info fw-bold">[Typing.com]</span></h5>
+        <p class="card-text">Typing.com ofreces varias tipos de pruebas (conometradas y de página) con el fin de
+          proporcionarte un máyor entorno de aprendicaje en este mundo que te ayudará a progresar con mayor rapidez. Los
+          resultados de esta prueba de teclado le darán tanto su velocidad de escritura como los resultados de precisión
+          de escritura.</p>
+        <br>
+        <div class="text-center">
+          <a href="https://www.typing.com/es/student/tests" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-12 col-md-5 m-0 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Pruebas de mecanografia <span class="text-info fw-bold">[Vedoque]</span></h5>
+        <p class="card-text">Este sitio web de ofrece tanto lecciones como cursos de mecanografía en el cual podrás
+          aprender desde los más básico hasta lo más avanzado. tiene diferentes tios de pruebas (velocidad, página, sin
+          errores) que te ayudarán mucho a progresar. Tiene un sitema de estadísticas en el que no es necesario que te
+          crees una cuenta e inicies sesión.</p><br>
+        <div class="text-center">
+          <a href="https://www.vedoque.com/html5/mecanografia/MecanoPruebas/" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- <div class="row m-0 align-items-center">
+  <div class="col-12 col-md-5 offset-md-1 mb-3">
+    <div class="card card-bg-tech">
+      <div class="card-body p-4">
+        <h5 class="card-title">Palabras estrella <span class="text-info fw-bold">[Agile Fingers]</span></h5>
+        <p class="card-text">Aprenda a escribir palabras completas. Consigue tantas estrellas como puedas escribir las
+          palabras escritas en ellas. Este juego de mecanografía se trata de velocidad y de conseguir tantos puntos como
+          sea posible. Escribe las palabras asignadas a las estrellas antes de que exploten.</p><br>
+        <div class="text-center">
+          <a href="https://agilefingers.com/es/juegos/palabras-estrella" target="_blank" rel="noopener noreferrer"
+            class="btnMenu">Acceder</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
