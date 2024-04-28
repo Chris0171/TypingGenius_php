da3dad77d1f2ab7541dd97f07d8ace1198a36bde
Christian Milanes <christianmilanes01@gmail.com>
2024-04-28 02:31:24 +0200

    faq page created

---
+++ faq.php
@@ -0,0 +1,91 @@
+<!-- Head start -->
+<?php
+include ('includes/head_start.inc.php');
+?>
+
+<!-- Imports -->
+<link rel="stylesheet" href="assets/CSS/styles.css" />
+
+<?php
+include ('includes/head_end.inc.php');
+?>
+
+<div class="container-fluid p-0 g-0">
+  <!-- Header -->
+  <?php
+  include ('includes/header.inc.php');
+  ?>
+  <!-- page content -->
+  <div class="row justify-content-center align-items-center m-0 mt-5">
+    <div class="col-12 col-md-10 card-bg-tech p-5">
+      <h2 class="text-warning fw-bold mb-3">Preguntas frecuentes</h2>
+      <ol>
+        <li>
+          <h4>¿Qué es TypingGenius?</h4>
+          <p><span class="text-info">TypingGenius</span> es una plataforma en línea diseñada para proporcionar
+            información y herramientas relacionadas con la práctica de mecanografía.</p>
+        </li>
+        <li>
+          <h4>¿Cómo puedo comenzar a usar TypingGenius?</h4>
+          <p>Para comenzar a usar <span class="text-info">TypingGenius</span>, simplemente visite nuestra página de
+            inicio y explore las diferentes secciones y herramientas disponibles.</p>
+        </li>
+        <li>
+          <h4>¿Necesito crear una cuenta para utilizar TypingGenius?</h4>
+          <p>No es necesario crear una cuenta para acceder y utilizar la mayoría de las funciones de <span
+              class="text-info">TypingGenius</span>. Sin embargo, puede ser necesario proporcionar un alias para ciertas
+            características, como el área de práctica.</p>
+        </li>
+        <li>
+          <h4>¿Qué tipo de información se recopila cuando uso TypingGenius?</h4>
+          <p><span class="text-info">TypingGenius</span> solo recopila la información necesaria para brindarle servicios
+            y mejorar su experiencia de usuario, como su alias en el área de práctica. Consulte nuestra Política de
+            Privacidad para obtener más detalles.</p>
+        </li>
+        <li>
+          <h4>¿Cómo puedo cambiar mi alias en el área de práctica?</h4>
+          <p>Para cambiar su alias en el área de práctica, simplemente visite la sección de configuración de su cuenta
+            (si está disponible) o comuníquese con nuestro equipo de soporte para obtener ayuda.</p>
+        </li>
+        <li>
+          <h4>¿Qué sucede si olvido mi alias en el área de práctica?</h4>
+          <p>Si olvida su alias en el área de práctica, puede comunicarse con nuestro equipo de soporte para obtener
+            ayuda para recuperarlo o crear uno nuevo.</p>
+        </li>
+        <li>
+          <h4>¿Ofrecen tutoriales o consejos para mejorar mi velocidad y precisión de mecanografía?</h4>
+          <p>Sí, <span class="text-info">TypingGenius</span> ofrece tutoriales, consejos y recursos para ayudarlo a
+            mejorar su velocidad y precisión de mecanografía. Explore nuestra sección de recursos para obtener más
+            información.</p>
+        </li>
+        <li>
+          <h4>¿Es seguro proporcionar mi información personal en TypingGenius?</h4>
+          <p>Sí, <span class="text-info">TypingGenius</span> se compromete a proteger su privacidad y seguridad.
+            Consulte nuestra Política de Privacidad para obtener más detalles sobre cómo protegemos su información
+            personal.</p>
+        </li>
+        <li>
+          <h4>¿Puedo compartir mi progreso en las redes sociales desde TypingGenius?</h4>
+          <p>Sí, muchas de nuestras funciones permiten compartir su progreso en las redes sociales si así lo desea. Sin
+            embargo, siempre respetamos su privacidad y no compartiremos su información sin su consentimiento.</p>
+        </li>
+        <li>
+          <h4>¿Cómo puedo ponerme en contacto con el equipo de soporte de TypingGenius?</h4>
+          <p>Puede ponerse en contacto con nuestro equipo de soporte enviando un correo electrónico a
+            <span class="text-info">christianmilanes01@gmail.com </span>o utilizando el formulario de contacto en
+            nuestro sitio web.
+          </p>
+        </li>
+      </ol>
+    </div>
+  </div>
+  <!-- Footer -->
+  <?php
+  include ('includes/footer.inc.php');
+  ?>
+</div>
+
+<!-- End -->
+<?php
+include ('includes/end.inc.php');
+?>
