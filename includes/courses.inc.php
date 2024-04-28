e7ed42764c3c99e7456d3c97504e5b9925eddc37
Christian Milanes <christianmilanes01@gmail.com>
2024-04-28 03:17:34 +0200

    courses inc updated

--- includes/courses.inc.php
+++ includes/courses.inc.php
@@ -6,7 +6,7 @@
 <div class="row justify-content-center m-0 align-items-center">
   <div class="col-12 col-md-5 m-0 mb-3">
     <div class="card card-bg-tech">
-      <div class="card-body">
+      <div class="card-body p-4">
         <h5 class="card-title">Curse de mecanografía <span class="text-info fw-bold">[Artypist]</span></h5>
         <p class="card-text">Este curso te enseñará las claves para ser un maestro de la mecanografía. Aprenderás las
           tres reglas básicas que siempre debes recordar, aprenderás a colocar los dedos en el teclado y practicarás con
@@ -14,14 +14,14 @@
           tu progreso.</p><br>
         <div class="text-center">
           <a href="https://www.artypist.com/es/curso-mecanografia/practicar" target="_blank" rel="noopener noreferrer"
-            class="btnMenu">Visitar el sitio</a>
+            class="btnMenu">Visitar sitio</a>
         </div>
       </div>
     </div>
   </div>
   <div class="col-12 col-md-5 m-0 mb-3">
     <div class="card card-bg-tech">
-      <div class="card-body">
+      <div class="card-body p-4">
         <h5 class="card-title">Curso de mecanografía <span class="text-info fw-bold">[Agile Fingers]</span></h5>
         <p class="card-text">El curso de mecanografia comienza con ejercicios fundamentales que perfeccionan la técnica
           de mecanografía utilizando un grupo específico de teclas que de alguna manera están relacionadas: están una al
@@ -29,7 +29,7 @@
           teclas numéricas.</p>
         <div class="text-center"><br>
           <a href="https://agilefingers.com/es/curso" target="_blank" rel="noopener noreferrer" class="btnMenu">Visitar
-            el sitio</a>
+            sitio</a>
         </div>
       </div>
     </div>
@@ -38,13 +38,13 @@
 <div class="row m-0 align-items-center">
   <div class="col-12 col-md-5 offset-md-1 mb-3">
     <div class="card card-bg-tech">
-      <div class="card-body">
+      <div class="card-body p-4">
         <h5 class="card-title">Lecciones de mecanografía <span class="text-info fw-bold">[TypingStudy]</span></h5>
         <p class="card-text">Este curso ofrece <span class="text-warning">15</span> lecciones con soporte en muchos
           idiomas para practicar cuanto quieras. Ofrece pruebas de velocidad, algunos juegos que promueven la constancia
           y las buenas prácticas junto a la posición. Adicionalmente posee una sección de teclado numérico.</p><br>
         <div class="text-center">
-          <a href="https://www.typingstudy.com/es/" target="_blank" rel="noopener noreferrer" class="btnMenu">Visitar el
+          <a href="https://www.typingstudy.com/es/" target="_blank" rel="noopener noreferrer" class="btnMenu">Visitar
             sitio</a>
         </div>
       </div>
