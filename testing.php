9ee19addc722f23624978edf811bdc17eb6becbc
Christian Milanes <christianmilanes01@gmail.com>
2024-04-28 11:33:02 +0200

    testing page created

---
+++ testing.php
@@ -0,0 +1,31 @@
+<!-- Head start -->
+<?php
+include ('includes/head_start.inc.php');
+?>
+
+<!-- Imports -->
+<link rel="stylesheet" href="assets/CSS/styles.css" />
+
+<?php
+include ('includes/head_end.inc.php');
+?>
+
+<div class="container-fluid p-0 g-0">
+  <!-- Header -->
+  <?php
+  include ('includes/header.inc.php');
+  ?>
+  <!-- page content -->
+  <?php
+  include ('includes/testing.inc.php');
+  ?>
+  <!-- Footer -->
+  <?php
+  include ('includes/footer.inc.php');
+  ?>
+</div>
+
+<!-- End -->
+<?php
+include ('includes/end.inc.php');
+?>
