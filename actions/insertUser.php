0860f7bf112fc3e505914ac2760cefb926536c02
Christian Milanes <christianmilanes01@gmail.com>
2024-05-02 18:52:38 +0200

    insert php script created

---
+++ actions/insertUser.php
@@ -0,0 +1,40 @@
+<?php
+
+include "../includes/toolkit/functions.php";
+include "../includes/db_connection.inc.php";
+
+$conn = new DbConnection("localhost", "root", "Codecreatoruserunknown01", "typinggenius_db");
+
+$conn->connect();
+
+//$alias = $_POST['alias'];
+//$level = $_POST['level'];
+//$errors = $_POST['errors'];
+
+$alias = "Unknown0";
+$level = 1;
+$errors = 24;
+
+$password = generatePass();
+
+echo $password;
+
+$passHash = password_hash($password, PASSWORD_DEFAULT);
+
+// Consulta SQL con marcadores de posición
+$query = "INSERT INTO Usuarios (alias, nivel, errores, pass) VALUES (?, ?, ?, ?)";
+
+// Preparar la consulta
+$stmt = $conn->getConn()->prepare($query);
+
+// Vincular parámetros
+$stmt->bind_param("siis", $alias, $level, $errors, $passHash);
+
+if ($stmt->execute()) {
+  echo "Inserción exitosa";
+} else {
+  echo "Error en la inserción: " . $conn->getConn()->error;
+}
+$stmt->close();
+
+?>
