a11ed16221948a4a892f691a75c631c0dccd9d41
Christian Milanes <christianmilanes01@gmail.com>
2024-05-03 19:08:23 +0200

    scores page created

---
+++ scores.php
@@ -0,0 +1,75 @@
+<!-- Head start -->
+<?php
+include ('includes/head_start.inc.php');
+?>
+
+<!-- Imports -->
+<link rel="stylesheet" href="assets/CSS/styles.css" />
+
+<?php
+include ('includes/head_end.inc.php');
+
+include "includes/toolkit/functions.php";
+include "includes/db_connection.inc.php";
+
+$conn = new DbConnection("localhost", "root", "Codecreatoruserunknown01", "typinggenius_db");
+
+$conn->connect();
+
+$allUsers = $conn->query("SELECT * FROM Usuarios");
+$rows = array();
+
+if ($allUsers) {
+  // Recorrer el conjunto de resultados para almacenar cada fila en el array
+  while ($row = $allUsers->fetch_assoc()) {
+    $rows[] = $row;
+  }
+  $uses = sortUsers($rows);
+}
+
+?>
+
+<div class="container-fluid p-0 g-0">
+  <!-- Header -->
+  <?php
+  include ('includes/header.inc.php');
+  ?>
+  <!-- page content -->
+  <div class="row justify-content-center m-0 align-items-center mt-5 mb-5">
+    <div class="col-12 col-md-10 col-lg-8 card-bg-tech p-5">
+      <div class="">
+        <table class="text-center p-3 text-white tableSt">
+          <thead class="">
+            <tr>
+              <th scope="col">Posición</th>
+              <th scope="col">Alias</th>
+              <th scope="col">Nivel</th>
+              <th scope="col">Errores</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            for ($i = 0; $i < count($uses); $i++) {
+              ?>
+            <tr>
+              <td><?php echo ($i + 1) ?></td>
+              <td><?php echo $uses[$i]['alias'] ?></td>
+              <td><?php echo $uses[$i]['nivel'] ?></td>
+              <td><?php echo $uses[$i]['errores'] ?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <!-- Footer -->
+  <?php
+  include ('includes/footer.inc.php');
+  ?>
+</div>
+
+<!-- End -->
+<?php
+include ('includes/end.inc.php');
+?>
