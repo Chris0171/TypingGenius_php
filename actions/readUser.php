8cd04066b1f8b2e2085e8cbe82e97f077bdfff79
Christian Milanes <christianmilanes01@gmail.com>
2024-05-04 00:33:44 +0200

    read user script created

---
+++ actions/readUser.php
@@ -0,0 +1,27 @@
+<?php
+
+include "../includes/toolkit/functions.php";
+include "../includes/db_connection.inc.php";
+
+$conn = new DbConnection("localhost", "root", "Codecreatoruserunknown01", "typinggenius_db");
+
+$conn->connect();
+
+$allUsers = $conn->query("SELECT alias FROM Usuarios");
+
+$rows = array();
+
+if ($allUsers) {
+  // Recorrer el conjunto de resultados para almacenar cada fila en el array
+  while ($row = $allUsers->fetch_assoc()) {
+    $rows[] = $row;
+  }
+
+  header('Content-Type: application/json');
+  echo json_encode($rows);
+} else {
+  header('Content-Type: application/json');
+  echo json_encode(array("error", "no se han encontrado usuarios..."));
+}
+
+?>
