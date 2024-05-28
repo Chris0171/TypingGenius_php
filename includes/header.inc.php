65d952fd73160ecfe38baed06d34a25d59a873e2
christianmilanes01 <christianmilanes01@gmail.com>
2024-05-28 15:28:46 +0000

    navbar updated by key words

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -2,8 +2,7 @@
   <!-- Navbar -->
   <nav class="container-xxl">
     <div class="homeLink">
-      <a class="navbar-brand fw-bold ms-2 ms-md-3" href="#"><img class="img-fluid d-inline-block"
-          src="assets/favicon.ico" width="50" alt="ContentSynth-Logo" width="85"></a>
+      <a class="navbar-brand fw-bold ms-2 ms-md-3" href="#"><img class="img-fluid d-inline-block" src="assets/favicon.ico" width="50" alt="ContentSynth-Logo" width="85"></a>
       <a href="index.php" class="appTitle">
         <p class="text-center m-0 fw-bold">TypingGenius</p>
       </a>
@@ -14,10 +13,53 @@
     <div class="navbar-collapse collapse" id="menu">
       <nav class="navbar-nav ms-md-auto">
         <a class="nav-link fw-bold" id="index" href="index.php">INICIO</a>
+        <!-- Dropdown "PRACTICAR" -->
+        <div class="dropdown">
+          <button class="nav-link fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PRACTICAR
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark bgDropdown p-2">
+            <li><a class="dropdown-item" href="#">Practicar la mecanografía</a></li>
+            <li><a class="dropdown-item" href="#">Ejercicios de mecanografía</a></li>
+            <hr class="text-warning border border-2">
+            <li><a class="dropdown-item" href="#">Intruciones de prueba</a></li>
+            <li><a class="dropdown-item" href="#">Mejores posiciones</a></li>
+          </ul>
+        </div>
+        <!-- Dropdown "CURSOS" -->
+        <div class="dropdown">
+          <button class="nav-link fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            CURSOS
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark bgDropdown p-2">
+            <li><a class="dropdown-item" href="#">Cursos de mecanografía</a></li>
+            <li><a class="dropdown-item" href="#">Mecanografía para principiantes</a></li>
+            <hr class="text-warning border border-2">
+            <li><a class="dropdown-item" href="#">Mecanografía juegos</a></li>
+          </ul>
+        </div>
+        <!-- Dropdown "PRUEBAS" -->
+        <div class="dropdown">
+          <button class="nav-link fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PRUEBAS
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark bgDropdown p-2">
+            <li><a class="dropdown-item" href="#">Pruebas de mecanografía</a></li>
+            <li><a class="dropdown-item" href="#">Medir velocidad de tipeo</a></li>
+          </ul>
+        </div>
+        <!-- Dropdown "RECURSOS" -->
+        <div class="dropdown">
+          <button class="nav-link fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            RECURSOS
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark bgDropdown p-2">
+            <li><a class="dropdown-item" href="#">Aprender mecanografía</a></li>
+            <li><a class="dropdown-item" href="#">Mecangrafía onlines</a></li>
+            <li><a class="dropdown-item" href="#">Escribir sin ver el teclado</a></li>
+          </ul>
+        </div>
         <a class="nav-link fw-bold" id="about-us" href="about-us.php">SOBRE NOSOTROS</a>
-        <a class="nav-link fw-bold" id="learn-more" href="learn-more.php">APRENDER MÁS</a>
-        <a class="nav-link fw-bold" id="instructions" href="instructions.php">INSTRUCCIONES</a>
-        <a class="nav-link fw-bold" id="scores" href="scores.php">MEJORES</a>
       </nav>
     </div>
   </nav>
