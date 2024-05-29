989060a0b55ed83a1403e1703d5ac01d769c3bb4
christianmilanes01 <christianmilanes01@gmail.com>
2024-05-29 12:22:39 +0000

    new content added

--- includes/index.inc.php
+++ includes/index.inc.php
@@ -8,19 +8,54 @@ session_start();
       $isOk = $_SESSION['isOk'];
       if ($isOk) {
         ?>
-    <div class="alert alert-success fw-bold">
-      Las estadísticas de usuario han sido actualizadas. ¡Disfrute de nuestro sitio web!
-    </div>
-    <?php
+        <div class="alert alert-success fw-bold">
+          Las estadísticas de usuario han sido actualizadas. ¡Disfrute de nuestro sitio web!
+        </div>
+        <?php
       } else {
         ?>
-    <div class="alert alert-danger fw-bold">
-      Las estadísticas de usuario no han sido actualizadas. Ha habido algún error. Lo solucionaremos en breve.
-    </div>
-    <?php
+        <div class="alert alert-danger fw-bold">
+          Las estadísticas de usuario no han sido actualizadas. Ha habido algún error. Lo solucionaremos en breve.
+        </div>
+        <?php
       }
     }
     ?>
+  </div>
+</div>
+
+<div class="row justify-content-center align-items-center m-0 mt-3">
+  <div class="col-11 col-md-10 mb-3 mb-md-0 text-white">
+    <div class="card-bg-tech p-4">
+      <h1 class="text-center text-warning">Fundamentos de la Mecanografía</h1> <br>
+      <h2>¿Por qué es tan importante aprender a escribir rápido y preciso?</h2>
+      <p>En un mundo cada vez más digitalizado, la habilidad de mecanografía se ha convertido en un activo invaluable.
+        ¿Por qué? Porque la capacidad de escribir rápidamente y con precisión en un teclado no solo es una destreza
+        técnica, sino también una herramienta esencial para el éxito en el ámbito laboral y académico. En este artículo,
+        exploraremos los fundamentos de la mecanografía y por qué es crucial dominarla en la era digital.</p>
+      <br>
+      <h2>Productividad y ergonomía</h2>
+      <p>Una de las principales razones para aprender mecanografía es el aumento significativo de la productividad que
+        ofrece. Imagina poder escribir un correo electrónico, un informe o incluso un mensaje de texto con rapidez y
+        eficiencia. Esta habilidad te permite completar tareas en menos tiempo, lo que te deja más espacio para abordar
+        otras responsabilidades o simplemente disfrutar de un merecido descanso.</p>
+      <br>
+      <p>Otro aspecto crucial de la mecanografía es su impacto en la ergonomía. Al aprender la técnica correcta de
+        posicionamiento de las manos en el teclado, puedes reducir la tensión en tus músculos y articulaciones, lo que a
+        su vez disminuye el riesgo de lesiones por esfuerzo repetitivo (RSI). Una postura adecuada y movimientos fluidos
+        son fundamentales para mantener la salud a largo plazo mientras trabajas en un entorno digital.</p>
+      <br>
+      <h2>Prevención de errores por esfuerzo repetitivo (RSI)</h2>
+      <p>El RSI es un problema común entre aquellos que pasan largas horas frente a una computadora. Los movimientos
+        repetitivos y la tensión muscular pueden causar dolores crónicos en las manos, muñecas y brazos. Sin embargo, al
+        dominar la mecanografía, puedes minimizar el riesgo de desarrollar estas lesiones, ya que aprenderás a escribir
+        con una técnica que reduce la tensión y el estrés en las articulaciones.</p>
+      <br>
+      <p>La mecanografía va más allá de simplemente aprender a escribir más rápido. Es una habilidad fundamental que
+        puede aumentar tu productividad, mejorar tu salud ergonómica y prevenir lesiones a largo plazo. En la era
+        digital en la que vivimos, dominar la mecanografía es una inversión en tu futuro laboral y bienestar personal.
+      </p>
+    </div>
   </div>
 </div>
 
