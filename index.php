430be924045e7268e9e646601f807453abd98240
christianmilanes01 <christianmilanes01@gmail.com>
2024-05-29 12:21:26 +0000

    meta tags added

--- index.php
+++ index.php
@@ -2,10 +2,38 @@
 <?php
 include ('includes/head_start.inc.php');
 ?>
+<!-- meta tags for SEO -->
+<title>Mecanografía | Inicio</title>
+<meta name="description"
+  content="TypingGenius. Bienvenidos a nuestro sitio web de mecanografía. Conoce todo detrás de esta habilidad imensamente demandada hoy en día dentro del sector tecnológico.">
+<meta name="robots" content="index, follow">
+<meta name="googlebot" content="index, follow">
+<link rel="canonical" href="https://tygenius.com/index.php">
+
+<!-- Open Graph para Facebook y otros -->
+<meta property="og:title" content="Mecanografía | Inicio">
+<meta property="og:description"
+  content="TypingGenius. Bienvenidos a nuestro sitio web de mecanografía. Conoce todo detrás de esta habilidad imensamente demandada hoy en día dentro del sector tecnológico.">
+<!-- <meta property="og:image" content="https://www.tusitio.com/images/homepage.jpg"> -->
+<meta property="og:url" content="https://tygenius.com/index.php">
+<meta property="og:type" content="website">
+
+<!-- Twitter Cards -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Bienvenidos a Nuestro Sitio Web - Palabras Clave Principales">
+<meta name="twitter:description"
+  content="TypingGenius. Bienvenidos a nuestro sitio web de mecanografía. Conoce todo detrás de esta habilidad imensamente demandada hoy en día dentro del sector tecnológico.">
+<!-- <meta name="twitter:image" content="https://www.tusitio.com/images/homepage.jpg"> -->
+
+<!-- Idioma y Geolocalización -->
+<meta name="language" content="es">
+<meta name="geo.region" content="ES-M">
+<meta name="geo.placename" content="Madrid">
 
 <!-- Imports -->
 <link rel="stylesheet" href="assets/CSS/styles.css" />
 
+<!-- Head end -->
 <?php
 include ('includes/head_end.inc.php');
 ?>
